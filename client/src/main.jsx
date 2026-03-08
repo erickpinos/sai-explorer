c8ff5a245bcac081cca475ace2b44b52508d8226
Erick Pinos <erick@erickpinos.com>
2026-03-08 11:06:44 -0400

    Added posthog

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import './index.css'
 import App from './App.jsx'
 
+const POSTHOG_KEY = import.meta.env.VITE_PUBLIC_POSTHOG_KEY
+const posthogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+  capture_pageview: true,
+  capture_pageleave: true,
+  autocapture: true,
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {POSTHOG_KEY ? (
+      <PostHogProvider apiKey={POSTHOG_KEY} options={posthogOptions}>
+        <App />
+      </PostHogProvider>
+    ) : (
+      <App />
+    )}
   </StrictMode>,
 )
